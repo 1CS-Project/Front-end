0504b93ec26a2ed413721067205a0a80c19d51f1
MohamedRami Benaouf <studentrami21@gmail.com>
2024-06-04 14:56:57 +0100

    cnd

--- src/components/landing/BeforeRegistration/steps/steps.tsx
+++ src/components/landing/BeforeRegistration/steps/steps.tsx
@@ -6,7 +6,7 @@ function Steps() {
   const t = useTranslations("journey");
   return (
     <section className='mt-[8rem] scroll-mt-20' id='steps-section'>
-      <h1 className="text-center font-semibold text-3xl mb-[4rem]">Unlock the journey to Mecca with these essential steps</h1>
+      <h1 className="text-center font-semibold text-3xl mb-[4rem]">{t("header")}</h1>
 
       <div className="w-full grid grid-cols-1 md:grid-cols-2 mx-auto gap-[10em] items-center justify-between mt-8">
         
@@ -43,9 +43,8 @@ function Steps() {
             </div>
       
         <div data-aos="fade-left" >
-        <h1 className="font-semibold text-xl mb-3">Examination</h1>
-          <p className=''>In this step, pilgrims are guided through the process of scheduling and attending medical examinations to assess their physical fitness and readiness for the rigorous journey ahead.
-In this step, pilgrims are guided through the process of scheduling and attending medical examinations to assess their physical fitness and readiness for the rigorous journey ahead.</p>
+        <h1 className="font-semibold text-xl mb-3">{t("exam")}</h1>
+          <p className=''>{t("examinfo")}</p>
       
           
         </div>
@@ -54,9 +53,8 @@ In this step, pilgrims are guided through the process of scheduling and attendin
         
         <div  data-aos="fade-right">
           
-          <h1 className="font-semibold text-xl mb-3">Accommodation</h1>
-          <p className=''>In this step, pilgrims are presented with a comprehensive list of available hotels, ranging from budget-friendly options to luxury accommodations, conveniently located near the holy sites.
- Each hotel listing includes detailed information such as amenities, room types, proximity to religious landmarks, and pricing options.</p>
+          <h1 className="font-semibold text-xl mb-3">{t("accom")}</h1>
+          <p className=''>{t("accominfo")}</p>
         </div>
         {/*<div className="textCircle absolute w-full h-full anim">
             {innerText.split('').map((char: string, i: number) => (
@@ -86,10 +84,8 @@ In this step, pilgrims are guided through the process of scheduling and attendin
             </div>
       
         <div data-aos="fade-left" >
-        <h1 className="font-semibold text-xl mb-3">Payment</h1>
-          <p className=''>In this step, pilgrims are provided with a secure and user-friendly platform to settle their bills and fees associated with the pilgrimage, including registration fees, accommodation costs, transportation expenses, and any additional charges. Pilgrims can conveniently view a detailed breakdown of their expenses, including itemized costs and any applicable taxes or surcharges.</p>
-      
-          
+        <h1 className="font-semibold text-xl mb-3">{t("pay")}</h1>
+          <p className=''>{t("payinfo")}</p>
         </div>
       </div>
 
@@ -97,8 +93,8 @@ In this step, pilgrims are guided through the process of scheduling and attendin
         
         <div  data-aos="fade-right">
           
-          <h1 className="font-semibold text-xl mb-3">Follow Your Hadj Step by Step</h1>
-          <p className=''>In this step, pilgrims gain access to a curated series of resources, tools, and information to navigate each stage of the Hajj pilgrimage with confidence and ease. From the initial preparations to the culmination of rituals in Mecca, pilgrims receive step-by-step instructions, practical tips, and spiritual guidance to ensure a meaningful and fulfilling experience.</p>
+          <h1 className="font-semibold text-xl mb-3">{t("step")}</h1>
+          <p className=''>{t("stepinfo")}</p>
         </div>
         {/*<div className="textCircle absolute w-full h-full anim">
             {innerText.split('').map((char: string, i: number) => (
